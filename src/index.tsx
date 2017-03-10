1071e5ec516e97a3673d99a75a081111820608f5
akasaka_akio <akasaka_akio@sgk.co.jp>
2017-03-10 19:24:45 +0900

    Add css loading to webpack.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import * as React from "react"; //load typescript define from react? or load libraries?
 import * as ReactDOM from "react-dom";
 import {Game} from "./components/reacttutorial/Game" //load components from components folder
-
-
+import "../delete.css";
 
 //React Tutorial
 ReactDOM.render(
--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,8 @@ module.exports = {
     module: {
         loaders: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+            { test: /\.css?$/, loader: "style-loader!css-loader" }
         ],
         //can not be identified
         // preLoaders: [
